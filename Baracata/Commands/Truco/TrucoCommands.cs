6893c704e0812b6870701293a8a9d41c08ca2b85
Luis <lfscruz@outlook.com>
2025-01-28 00:27:37 -0300

    bot sending dms

---
+++ Baracata/Commands/Truco/TrucoCommands.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using System.Threading.Tasks;
+
+namespace Baracata.Commands.Truco
+{
+    public class TrucoCommands : ApplicationCommandModule
+    {
+        [SlashCommand("truco", "iniciar um jogo de truco")]
+        public async Task InitializeTrucoCommand(InteractionContext ctx, [Option("player1","primeiro jogador")] DiscordUser player1, [Option("player2", "segundo jogador")] DiscordUser player2, [Option("player3", "terceiro jogador")] DiscordUser player3, [Option("player4", "quarto jogador")] DiscordUser player4) 
+        { 
+            await ctx.DeferAsync();
+            var players = new[] { (DiscordMember)player1, (DiscordMember)player2, (DiscordMember)player3, (DiscordMember)player4 };
+
+            foreach (DiscordMember player in players)
+            {
+                var DMChannel = await player.CreateDmChannelAsync();
+                await DMChannel.SendMessageAsync($"hello there {player.Username}");
+            }
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"{player1}, {player2}, {player3}, {player4}"));
+
+        }
+    }
+}
--- Baracata/Program.cs
+++ Baracata/Program.cs
@@ -1,18 +1,49 @@
-﻿using DotNetEnv;
-
+﻿using Baracata.Commands.Truco;
+using DotNetEnv;
+using DSharpPlus;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.SlashCommands;
 using System;
+using System.Threading.Tasks;
 
 namespace Baracata
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient Client { get; set; }
+        static async Task Main(string[] args)
         {
             Env.Load("E:\\Users\\lfscr\\source\\repos\\Baracata\\Baracata\\.env");
-            Console.WriteLine("Environment variables loaded.");
             string botToken = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
-            Console.WriteLine($"Token: {botToken ?? "No token found"}");
-            Console.ReadKey();
+
+            DiscordConfiguration discordConfiguration = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = botToken,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+            Client = new DiscordClient(discordConfiguration);
+
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            Client.Ready += Client_Ready;
+
+            SlashCommandsExtension slashCommandsConfig = Client.UseSlashCommands();
+            slashCommandsConfig.RegisterCommands<TrucoCommands>();
+
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
