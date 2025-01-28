8ed2ad1ed5404e44cf752919874c7517f538ce5a
Luis <lfscruz@outlook.com>
2025-01-27 22:31:42 -0300

    Add project files.

---
+++ Baracata/Program.cs
@@ -0,0 +1,17 @@
+﻿using DotNetEnv;
+
+using System;
+
+namespace Baracata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Env.Load("E:\\Users\\lfscr\\source\\repos\\Baracata\\Baracata\\.env");
+            Console.WriteLine("Environment variables loaded.");
+            string botToken = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+            Console.WriteLine($"Token: {botToken ?? "No token found"}");
+        }
+    }
+}
